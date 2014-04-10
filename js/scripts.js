5751d2921330193d702ddfaf838a0f31bd5af0ae
Tiago Costa <phb.tiago@gmail.com>
2014-04-10 16:25:58 +0100

    fixed banner image, ninja image and bottom links to make them more responsive.

--- js/scripts.js
+++ js/scripts.js
@@ -1,74 +1,33 @@
 $(document).ready(function(){
-	$('a[href^="#about"]').on('click',function (e) {
-	    e.preventDefault();
+	// <link-effect-transitions>
+	$('.link-effect').on('click',function (e) {
+		e.preventDefault();
 
-	    var target = this.hash,
-	    $target = $(target);
+		var target = this.hash,
+		$target = $(target);
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top
+		}, 900, 'swing', function () {
+			window.location.hash = target;
+		});
 	});
+	// </link-effect-transitions>
 });
 
-$(document).ready(function(){
-	$('a[href^="#education"]').on('click',function (e) {
-	    e.preventDefault();
 
-	    var target = this.hash,
-	    $target = $(target);
+$(window).load(function() {
+	var mq = window.matchMedia( "(min-width: 768px)" );
+	if (mq.matches) {
+		var margin = ($('#skills_row').height() - $('#ninja-div').height()) / 2;
+		$('#ninja-div').css('margin-top', margin);
+	}
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
+	mq = window.matchMedia( "(max-width: 486px)" );
+	if (mq.matches) {
+		$('#bottom-link-list').removeClass('list-inline');
+		$('#bottom-link-list').addClass('list-unstyled');
+		console.log("match");
+	}
 });
 
-$(document).ready(function(){
-	$('a[href^="#work"]').on('click',function (e) {
-	    e.preventDefault();
-
-	    var target = this.hash,
-	    $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
-});
-
-$(document).ready(function(){
-	$('a[href^="#skills"]').on('click',function (e) {
-	    e.preventDefault();
-
-	    var target = this.hash,
-	    $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
-});
-
-$(document).ready(function(){
-	$('a[href^="#contact"]').on('click',function (e) {
-	    e.preventDefault();
-
-	    var target = this.hash,
-	    $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
-});
